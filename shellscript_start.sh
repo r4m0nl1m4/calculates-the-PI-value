53a09423abfb2334267d732e6487544aeb69fe0c
r4m0nl1m4 <ramon.ccodes@gmail.com>
2020-11-15 15:47:32 -0300

    ADD efficiency

--- shellscript_start.sh
+++ shellscript_start.sh
@@ -4,22 +4,31 @@
 
 fileName0="README.txt"
 fileName1="result_report-cpu.txt"
-fileName2="result_report-serie-runtime.txt"
-fileName3="result_report-parallel-runtime.txt"
-fileName4="result_report-speedup.txt"
-fileName5="result_report-parallel-cpu.txt"
+fileName2="result_report-problem_size.txt"
+fileName3="result_report-serie-runtime.txt"
+fileName4="result_report-parallel-cores.txt"
+fileName5="result_report-parallel-runtime.txt"
+fileName6="result_report-speedup.txt"
+fileName7="result_report-efficiency.txt"
+fileName8="result_report-parallel-cpu.txt"
 
 fileHeader1="\n/* \n * CPU Report                         \n */\n"
-fileHeader2="\n/* \n * Serie Runtime Report In Seconds    \n */  "
-fileHeader3="\n/* \n * Parallel Runtime Report In Seconds \n */  "
-fileHeader4="\n/* \n * Speedup Report                     \n */  "
-fileHeader5="\n/* \n * Parallel Calculation Report        \n */  "
+fileHeader2="\n/* \n * Problem Size Report                \n */  "
+fileHeader3="\n/* \n * Serie Runtime Report In Seconds    \n */  "
+fileHeader4="\n/* \n * Parallel Cores Report              \n */  "
+fileHeader5="\n/* \n * Parallel Runtime Report In Seconds \n */  "
+fileHeader6="\n/* \n * Speedup Report                     \n */  "
+fileHeader7="\n/* \n * Efficiency Report                  \n */  "
+fileHeader8="\n/* \n * Parallel Calculation Report        \n */  "
 
 echo -e "$fileHeader1" >> $fileName1
 echo -e "$fileHeader2" >> $fileName2
 echo -e "$fileHeader3" >> $fileName3
 echo -e "$fileHeader4" >> $fileName4
 echo -e "$fileHeader5" >> $fileName5
+echo -e "$fileHeader6" >> $fileName6
+echo -e "$fileHeader7" >> $fileName7
+echo -e "$fileHeader8" >> $fileName8
 
 #serie compiler
 g++ -O0 -g -W -ansi -pedantic -std=c++11 -o calculates-the-pi-value-serial calculates-the-pi-value-serial.cpp
@@ -43,14 +52,18 @@ for cores in 2 4 6 8
 do 
 	for sizeProblem in 2000 4000
 	do 
-		serie=' '
-		echo -e "\n $serie $sizeProblem\t\c                        " >> $fileName2
-		echo -e "\n $cores $sizeProblem\t\c                        " >> $fileName3
-		echo -e "\n $cores $sizeProblem\t\c                        " >> $fileName4
-	    echo -e "\n $cores Cores CPU - Size Problem $sizeProblem \n" >> $fileName5
+		echo -e "" >> $fileName2
+		echo -e "" >> $fileName3
+		echo -e "" >> $fileName4
+		echo -e "" >> $fileName5
+		echo -e "" >> $fileName6
+		echo -e "" >> $fileName7
+	    echo -e "\n $cores Cores CPU - Size Problem $sizeProblem \n" >> $fileName8
 		for attempt in $(seq $attempts)
 		do
-			echo -e "  Try $attempt" >> $fileName5
+            echo -e ":$sizeProblem\c" >> $fileName2
+            echo -e ":$cores\c      " >> $fileName4
+			echo -e "  Try $attempt " >> $fileName8
 			#serie execute
 			./calculates-the-pi-value-serial $sizeProblem
 			#parallel execute
@@ -71,6 +84,9 @@ showOnTerminal $fileName1
 showOnTerminal $fileName2
 showOnTerminal $fileName3
 showOnTerminal $fileName4
+showOnTerminal $fileName5
+showOnTerminal $fileName6
+showOnTerminal $fileName7
 echo -e
 
 txt2pdf(){
@@ -82,11 +98,14 @@ txt2pdf $fileName2
 txt2pdf $fileName3
 txt2pdf $fileName4
 txt2pdf $fileName5
+txt2pdf $fileName6
+txt2pdf $fileName7
+txt2pdf $fileName8
 
-pdfunite $fileName0.pdf $fileName1.pdf $fileName2.pdf $fileName3.pdf $fileName4.pdf $fileName5.pdf report.pdf
+pdfunite $fileName0.pdf $fileName1.pdf $fileName2.pdf $fileName3.pdf $fileName4.pdf $fileName5.pdf $fileName6.pdf $fileName7.pdf $fileName8.pdf report.pdf
 
-rm $fileName0.pdf $fileName1.pdf $fileName2.pdf $fileName3.pdf $fileName4.pdf $fileName5.pdf
-rm $fileName1 $fileName2 $fileName3 $fileName4 $fileName5
+rm $fileName0.pdf $fileName1.pdf $fileName2.pdf $fileName3.pdf $fileName4.pdf $fileName5.pdf $fileName6.pdf $fileName7.pdf $fileName8.pdf
+rm $fileName1 $fileName2 $fileName3 $fileName4 $fileName5 $fileName6 $fileName7 $fileName8
 rm calculates-the-pi-value-serial
 rm calculates-the-pi-value-parallel
 rm calculates-serie-parallel-analysis
